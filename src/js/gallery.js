4ed60a679cffcbc245b2dfa6b681902cd675eb66
Anastasiya <anastasiya.anast.ru@mail.ru>
2024-05-22 17:23:45 +0300

    feat: add galleru btn js

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,6 +1,17 @@
 import birdsDataEn from './dataEN.js';
 import birdsData from './dataRU.js';
 
-console.log(birdsDataEn);
+const galleryBtns = document.getElementById('gallery__btns');
+const galleryBtn = document.querySelectorAll('.gallery__btn');
 
-console.log(birdsData);
+
+galleryBtns.addEventListener('click', function(el){
+  galleryBtn.forEach(function(e){
+    e.classList.remove('gallery__btn__active');
+  })
+
+  if(el.target.classList.contains('gallery__btn')) {
+    el.target.classList.add('gallery__btn__active');
+  }
+})
+
